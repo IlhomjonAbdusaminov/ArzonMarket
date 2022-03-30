0a988a6d060579559f5467f74bb05ef0072d7c4e
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-30 09:42:43 +0500

    IGenericRepository Imlemented

--- ArzonMarket.Data/Repositories/GenericRepository.cs
+++ ArzonMarket.Data/Repositories/GenericRepository.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using ArzonMarket.Data.IRepositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArzonMarket.Data.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
