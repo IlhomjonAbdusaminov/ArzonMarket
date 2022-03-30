9a5dc6a3f9bbcb208086ec60b66fde8ca0b5c794
Ilhomjon Abdusaminov <abdusaminovilhomjon30037007@gmail.com>
2022-03-30 09:40:32 +0500

    IGenericRepositoryFilled

--- ArzonMarket.Data/IRepositories/IGenericRepository.cs
+++ ArzonMarket.Data/IRepositories/IGenericRepository.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArzonMarket.Data.IRepositories
 {
-    public class IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        Task<T> CreateAsync(T entity);
+
+        Task<bool> DeleteAsync(Expression<Func<T, bool>> predicate);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate);
     }
 }
